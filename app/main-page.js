7b84cfe5844a375658f073ec616207862fe82b73
nikolay-diyanov <nikolay.diyanov@telerik.com>
2015-11-23 13:28:08 +0200

    size and delegate

--- app/main-page.js
+++ app/main-page.js
@@ -11,6 +11,7 @@ function pageLoaded(args) {
     if(platformModule.device.os == "iOS") {
         bannerView = placeholder.ios;
         bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716";
+        bannerView.strongDelegateRef = bannerView.delegate = GADBannerViewDelegateImpl.new().initWithOwner(this);
         bannerView.rootViewController = page.ios;
         var request = GADRequest.request();
         request.testDevices = [kGADSimulatorID];
@@ -20,10 +21,30 @@ function pageLoaded(args) {
 
 function creatingView(args) {
     if(platformModule.device.os == "iOS") {
-        bannerView = GADBannerView.alloc().init();
+        bannerView = GADBannerView.alloc().initWithAdSize(kGADAdSizeSmartBannerPortrait);
         args.view = bannerView;
     }
 }
+
+var GADBannerViewDelegateImpl = (function (_super) {
+    __extends(GADBannerViewDelegateImpl, _super);
+    function GADBannerViewDelegateImpl() {
+        _super.apply(this, arguments);
+    }
+    GADBannerViewDelegateImpl.new = function () {
+        return _super.new.call(this);
+    };
+    GADBannerViewDelegateImpl.prototype.initWithOwner = function (owner) {
+        this._owner = owner;
+        return this;
+    };
+    GADBannerViewDelegateImpl.prototype.adViewWillLeaveApplication = function (bannerView) {
+    	// do sth as the user is leaving the app, because of a clicked ad
+        console.log("Leaving the app, bye bye!");
+    };
+    GADBannerViewDelegateImpl.ObjCProtocols = [GADBannerViewDelegate];
+    return GADBannerViewDelegateImpl;
+})(NSObject);
 
 exports.pageLoaded = pageLoaded;
 exports.creatingView = creatingView;
